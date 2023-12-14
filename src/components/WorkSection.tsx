4f39cfccff7bd290b38f8eca0986c0f843b511d6
Dipankar Prasad <iamdpunkr@gmail.com>
2023-12-14 18:40:50 +0530

    feat: add Carousel section

---
+++ src/components/WorkSection.tsx
@@ -0,0 +1,10 @@
+
+const WorkSection = () => {
+  return (
+    <section className="mx-[90px] rounded-3xl shadow border border-gray-200">
+        <h1 className="mt-[52px] text-center text-gray-800 text-5xl font-satoshi-bold leading-10">How it works</h1>
+    </section> 
+  )
+}
+
+export default WorkSection
